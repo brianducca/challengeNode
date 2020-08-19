2b27762321b0f1138a1eba71791901f61fee5990
Brian Ducca <bducca@agn.gov.ar>
2020-08-19 18:48:37 -0300

    Added API entry point

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.send("Docker con livereload");
-});
 
 app.listen(PORT, function(req, res) {
     console.log("API running on port:" + PORT);
